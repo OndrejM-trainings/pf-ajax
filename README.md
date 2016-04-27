bb348786bb608e77484d83008cd558673eaeb1d3
ondro <ondro@ondro-lenovo-flex>
2016-04-27 16:02:17 +0200

    README - odkazy

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Odkazy
+
+Primefaces tutorial:
+http://www.coreservlets.com/JSF-Tutorial/primefaces/#String-Input-2
+
+JSF tutorial:
+http://www.coreservlets.com/JSF-Tutorial/jsf2/#Ajax
--- src/main/java/pfajax/PageView.java
+++ src/main/java/pfajax/PageView.java
@@ -1,5 +1,6 @@
 package pfajax;
 
+import java.util.*;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
@@ -8,17 +9,40 @@ import javax.inject.Named;
 public class PageView {
 
     private String pozdrav = "Ahoj";
+    private List<String> pozdravy = new ArrayList<>();
 
     public String getPozdrav() {
         return pozdrav;
     }
 
     public void setPozdrav(String pozdrav) {
-        this.pozdrav = pozdrav + "!";
+        this.pozdrav = pozdrav;
+    }
+    
+    public void pridajVykricnik() {
+        pozdrav += "?";
+//        return "index.xhtml";
+    }
+
+    public void pridajX() {
+        pozdrav += "x";
+//        return "index.xhtml";
     }
 
     public String getAkcia() {
         return "Odošli";
+    }
+
+    public List<String> getPozdravy() {
+        return pozdravy;
+    }
+
+    public void setPozdravy(List<String> pozdravy) {
+        this.pozdravy = pozdravy;
+    }
+    
+    public List<String> moznosti(String value) {
+        return Arrays.asList("1", "2", "3", value);
     }
 
 }
